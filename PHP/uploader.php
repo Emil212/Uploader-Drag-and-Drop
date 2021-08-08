f4fa73376f6df04d4f930842352cc156ebc1ee75
Emiliano Ceron <hceronj1400@alumno.ipn.mx>
2021-08-08 08:24:01 -0500

    Fix bug destination

--- PHP/uploader.php
+++ PHP/uploader.php
@@ -5,7 +5,7 @@ if(isset($_FILES["file"])){
     $name = $_FILES["file"]["name"]; //Guarda el nombre del archivo que se esta subiendo 
     $file = $_FILES["file"]["tmp_name"]; //Guara el nombre del archivo que se esta subiendo e
     $error = $_FILES["file"]["error"]; //Para guardar el error
-    $destination  = "C:\laragon\www\Uploader-Drag-and-Drop\FILES" . $name;
+    $destination  = "../FILES/" . $name;
  
     $upload = move_uploaded_file($file, $destination);
     
